8f606c540ea1c05def354702f2fb9e331c0b35ac
Aidan <aidgal2@gmail.com>
2021-04-11 16:23:48 +0100

    Add clang-format config file. Run clang-format over repo.

--- SearchRingBuffer/SearchRingBuffer.h
+++ SearchRingBuffer/SearchRingBuffer.h
@@ -1,11 +1,11 @@
 #ifndef CONCURRENT_SEARCHRINGBUFFER_H
 #define CONCURRENT_SEARCHRINGBUFFER_H
 
+#include <algorithm>
 #include <array>
 #include <chrono>
 #include <mutex>
 #include <shared_mutex>
-#include <algorithm>
 
 namespace Concurrent
 {
@@ -182,10 +182,11 @@ SearchRingBuffer<T, SIZE>::findItem(const time_point requestedTime) const
   // Binary search using std::lower_bound to find the first element which does not compare less than requestedTime
   // Use std::lower_bound and use a binary predicate comparison which will return true is the first element is less than the second
   // Comparison lambda will use the requestedTime (pair.first) to compare
-  T    item;  // Fake object needed to construct target object.
-  auto above_val = std::lower_bound(start_arr, end_arr, std::make_pair(requestedTime, item), [](std::pair<time_point, T> lhs, std::pair<time_point, T> rhs) -> bool {
-    return lhs.first < rhs.first;
-  });
+  T    item; // Fake object needed to construct target object.
+  auto above_val =
+    std::lower_bound(start_arr, end_arr, std::make_pair(requestedTime, item), [](std::pair<time_point, T> lhs, std::pair<time_point, T> rhs) -> bool {
+      return lhs.first < rhs.first;
+    });
 
   if (above_val->first == requestedTime)
   {
--- SearchRingBuffer/Tests/MultiThreadedTests.cpp
+++ SearchRingBuffer/Tests/MultiThreadedTests.cpp
@@ -6,37 +6,36 @@ using minutes = std::chrono::minutes;
 
 SCENARIO("Populate the buffer with one thread and read from it from another")
 {
-        GIVEN("A circular buffer")
+  GIVEN("A circular buffer")
+  {
+    Concurrent::SearchRingBuffer<int, 10000> circBuff;
+
+    WHEN("A thread is populating the buffer")
+    {
+      auto time = sysClock::now();
+
+      std::thread producer([&circBuff, time]() {
+        for (int i = 0; i < 10000; ++i)
         {
-                Concurrent::SearchRingBuffer<int, 10000> circBuff;
-
-                WHEN("A thread is populating the buffer")
-                {
-                        auto time = sysClock::now();
-
-                        std::thread producer([&circBuff, time]() {
-                                for (int i = 0; i < 10000; ++i)
-                                {
-                                        circBuff.push((time + minutes(i)), i);
-                                }
-                        });
-
-                        // Sleep to give the publisher a head start.
-                        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-                        THEN("Another thread can concurrently read from the buffer")
-                        {
-                                std::thread consumer([&circBuff, time]() {
-                                        for (int i=0; i < 10000; ++i)
-                                        {
-
-                                                CHECK(i == circBuff.read(time + minutes(i)));
-                                        }
-                                });
-                                consumer.join();
-                        }
-                        producer.join();
-
-                }
+          circBuff.push((time + minutes(i)), i);
         }
+      });
+
+      // Sleep to give the publisher a head start.
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+      THEN("Another thread can concurrently read from the buffer")
+      {
+        std::thread consumer([&circBuff, time]() {
+          for (int i = 0; i < 10000; ++i)
+          {
+
+            CHECK(i == circBuff.read(time + minutes(i)));
+          }
+        });
+        consumer.join();
+      }
+      producer.join();
+    }
+  }
 }
